f8f41cda01551bca84c58a57c7096a2508ebf8c8
Zainyest <kadennewport@gmail.com>
2025-11-19 17:01:29 -0800

    Add Datagen

---
+++ src/client/java/zainyest/ethercore/datagen/EtherCoreDataGenerator.java
@@ -0,0 +1,11 @@
+package zainyest.ethercore.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EtherCoreDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
