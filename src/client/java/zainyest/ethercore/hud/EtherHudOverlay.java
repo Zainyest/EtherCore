849262192ec11ed207000bf4e0d4de4372d21765
Zainyest <kadennewport@gmail.com>
2025-12-08 21:43:38 -0800

    Add poe2-like ether and mental pool hud element

--- src/client/java/zainyest/ethercore/hud/EtherHudOverlay.java
+++ src/client/java/zainyest/ethercore/hud/EtherHudOverlay.java
@@ -8,6 +8,7 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.core.pattern.NotANumber;
 import zainyest.ethercore.EtherCore;
 import zainyest.ethercore.EtherCoreClient;
 import zainyest.ethercore.etherpool.EtherPoolView;
@@ -44,5 +45,19 @@ public class EtherHudOverlay {
         context.drawTexture(RenderPipelines.GUI_TEXTURED, ETHER_TEXTURE, 4, 24, 0, 5, (int) (182*etherPercentFilled), 5, 182,10);
         context.drawTexture(RenderPipelines.GUI_TEXTURED, ETHER_TEXTURE, 4, 24, 0, 0, 182, 5, 182,10);
         context.drawTextWithShadow(client.textRenderer, Text.literal(ether.val() + "/" + ether.max()).fillStyle(Style.EMPTY.withColor(Formatting.AQUA)), 188, 24, 0xff7fff7f);
+
+        int x = 2;
+        int y = context.getScaledWindowHeight() - 42;
+        // render poe-like ether pool and mental energy pool
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, Identifier.of(EtherCore.MOD_ID, "textures/gui/mentalpool_and_etherpool/border.png"), x, y, 0, 0, 40, 40, 40, 40);
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, Identifier.of(EtherCore.MOD_ID, "textures/gui/mentalpool_and_etherpool/semicircle_border.png"), x, y, 0, 0, 40, 40, 40, 40);
+
+        int etherTextureNum = Float.isNaN(etherPercentFilled) ? 27 : Math.clamp(Math.round((1-etherPercentFilled) * 27), 1, 27);
+        Identifier etherFilledTexture = Identifier.of(EtherCore.MOD_ID, "textures/gui/mentalpool_and_etherpool/" + "main_pool" + etherTextureNum + ".png");
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, etherFilledTexture, x, y, 0, 0, 40, 40, 40, 40);
+
+        int mentalTextureNum = Float.isNaN(mentalEnergyPercentFilled) ? 53 : Math.clamp(Math.round((1-mentalEnergyPercentFilled) * 53), 1, 53);
+        Identifier mentalFilledTexture = Identifier.of(EtherCore.MOD_ID, "textures/gui/mentalpool_and_etherpool/" + "semicircle_pool" + mentalTextureNum + ".png");
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, mentalFilledTexture, x, y, 0, 0, 40, 40, 40, 40);
     }
 }
